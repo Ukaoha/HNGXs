644ba3b8368decf2974ea8e5342ddc05ad87d0ff
ukaoha <ukaohachizoba6@gmail.com>
2023-09-08 21:02:13 +0100

    commit

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const UTC = ()=>{
-    const now = Date.now()
-    return now;
-}
+const day = document.querySelector('[data-testid=currentDayOfTheWeek]')
+const currentTime = document.querySelector('[data-testid=currentUTCTime')
 
-function currentDay(){
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-    const date = new Date();
-    const currentDay = days[date.getUTCDay()];
-    return currentDay;
-} 
+const date = new Date()
 
+const weekDays = [
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+  'Saturday',
+]
 
-const Today = document.getElementById('day');
-const time = document.getElementById('time')
-Today.textContent = currentDay();
-time.textContent = UTC();
+day.textContent = weekDays[date.getUTCDay()]
+currentTime.textContent = date.getTime()
